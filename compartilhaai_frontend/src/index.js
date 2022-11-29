f9991d50c102a562d5f430ed99620edf91c97d7d
Renan Johny Ortega <renanortega82@gmail.com>
2022-11-29 18:14:13 -0300

    refactor: use react dom create root

--- compartilhaai_frontend/src/index.js
+++ compartilhaai_frontend/src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+root.render(
+  <React.StrictMode>
     <GoogleOAuthProvider clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}>
-        <Router>
-            <App />
-        </Router>
-    </GoogleOAuthProvider>,
-    document.getElementById('root')
-);
+      <Router>
+        <App />
+      </Router>
+    </GoogleOAuthProvider>
+  </React.StrictMode>
+)
